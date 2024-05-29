2ff61f182278e0f2c078f910ca2a865bccb5cf75
Mauricio Toledo <mauricionrk@gmail.com>
2024-05-29 10:49:43 -0300

    fix: api docs

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('api')->group(function () {
-    Route::post('/conta', [AccountController::class, 'create']);
-    Route::get('/conta/{numero_conta}', [AccountController::class, 'getByNumber']);
+Route::get('/docs', function () {
+    return redirect('https://documenter.getpostman.com/view/31039731/2sA3QterTJ');
 });
